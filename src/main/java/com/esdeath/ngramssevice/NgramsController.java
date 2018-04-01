80ee9f442a98f5542acd23f3b2f0fbdc3eff2c36
EsdeathYZH <1162526220@qq.com>
2018-04-02 07:11:31 +0800

    Add log4j2 dependency for printing logs.

--- src/main/java/com/esdeath/ngramssevice/NgramsController.java
+++ src/main/java/com/esdeath/ngramssevice/NgramsController.java
@@ -12,7 +12,9 @@ public class NgramsController {
     }
 
     @RequestMapping(value = "/randomwrite",method = RequestMethod.GET)
-    public String random(String value_n, String word_num, String filename){
+    public String random(@RequestParam("value_n") String value_n,@RequestParam("word_num") String word_num,
+                         @RequestParam("filename") String filename){
+        NgramsSeviceApplication.logger.info("Successfully generate texts!");
         int n = Integer.parseInt(value_n);
         int num = Integer.parseInt(word_num);
         return Ngrams.generateText(n,num,filename);
